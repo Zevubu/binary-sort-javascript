ea3f876fe55a5744a68a40b6d6c1b88fc5f4a79e
Zev Hoffman <zevubu@gmail.com>
2020-01-29 18:30:14 -0800

    add js file and sudocoded algerithim

---
+++ index.js
@@ -0,0 +1,12 @@
+//  A pre-sorted array : const
+
+// A copy of presorted array to edit.
+
+// a variable to add
+
+
+// for function to array.length
+
+// spit the array into two.
+
+// console.log  array copy
