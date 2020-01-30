a829ef772798f154da8318dfd5bbd6a4523ddac4
Zev Hoffman <zevubu@gmail.com>
2020-01-29 18:58:25 -0800

    add js file and sudocoded algerithim

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 //  A pre-sorted array : const
 
+const array = [1,2,3,5,6]
+
 // A copy of presorted array to edit.
 
 // a variable to add
 
+const item = 4;
 
 // for function to array.length
 
-// spit the array into two.
+const numSearch = (arr, x) =>{
+    // find midle point and compare.
+
+    for(let i= 0; i < arr.length; i++){
+        if() 
+
+    }
+
+
 
 // console.log  array copy
+
+}
+
+numSearch(array, item)
+
+
