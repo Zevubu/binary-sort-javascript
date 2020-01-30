e52bf75eff75371759253d26b647ac449431fbc0
Zev Hoffman <zevubu@gmail.com>
2020-01-29 19:18:13 -0800

    added start and end items

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //  A pre-sorted array : const
 
-const array = [1,2,3,5,6]
+const array = [1,2,3,4,5,6]
 
 // A copy of presorted array to edit.
 
@@ -8,13 +8,26 @@ const array = [1,2,3,5,6]
 
 const item = 4;
 
+
 // for function to array.length
 
 const numSearch = (arr, x) =>{
+
+    let start = arr[0];
+    let end = arr[arr.length -1];
+    let mid = Math.floor[(start + end) / 2]
+
+    let arr = arr;
     // find midle point and compare.
 
     for(let i= 0; i < arr.length; i++){
-        if() 
+        if(x > arr[arr.length / 2]){
+            
+
+        }
+        else if (x < arr[arr.length / 2 + 1]){
+
+        }
 
     }
 
